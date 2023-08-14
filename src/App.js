f6099ab8480e2c81c35e4efeae7dc1c8307b86c4
muzammilk03 <muzammilkappan@gmail.com>
2023-08-14 16:08:13 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import Nevbar from "./components/Nevbar";
 import Home from "./Pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./Pages/Login";
+
+import Account from "./Pages/Account";
+import Signup from "./Pages/Signup";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
-    <Nevbar/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-    </Routes>
+      <AuthContextProvider>
+        <Nevbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<ProtectedRoute><Account /></ProtectedRoute>} />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,7 +12,16 @@ const Main = () => {
       setMovies(Response.data.results);
     });
   }, []);
-  console.log(movie);
+ 
+
+  const truncateString = (str,num)=>{
+    if(str?.length>num){
+      return str.slice(0,num)+'...';
+    }else{
+      return str;
+    }
+  };
+
 
   return (
     <div className="w-full h-[550px] text-white">
@@ -34,7 +43,8 @@ const Main = () => {
             </button>
           </div>
           <p className="text-gray-400 text-sm">Released: {movie?.release_date}</p>
-          <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">{movie?.overview}</p>
+          <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+            {truncateString(movie?.overview,150)}</p>
         </div>
       </div>
     </div>
